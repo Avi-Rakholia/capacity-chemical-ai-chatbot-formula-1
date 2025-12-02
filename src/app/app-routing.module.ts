932a8cc0b752c87f60713ccd8d2e9d5f5adc7a32
Vishakha Mishra <vkmishra@nsight-inc.com>
2025-12-02 21:03:57 +0530

    adding analytics page for admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,59 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginPath } from './shared/path-enums/login-path';
 import { CreateAccountPath } from './shared/path-enums/create-account-path';
+
 import { LoginComponent } from './pages/login/login.component';
 import { CreateAccount } from './pages/create-account/create-account.component';
 import { PendingApprovalsComponent } from './pages/pending-approval/pending-approvals.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
+import { AnalyticsComponent } from './pages/analytics/analytics.component';   // ⭐ NEW IMPORT
 
 
 export const routes: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: LoginPath.login
-    },
-    {
-        path: LoginPath.login,
-        pathMatch: 'full',
-        component: LoginComponent
-    },
-    {
-        path: CreateAccountPath.createAccount,
-        pathMatch: 'full',
-        component: CreateAccount
-    },
 
-    {
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: LoginPath.login
+  },
+
+  {
+    path: LoginPath.login,
+    pathMatch: 'full',
+    component: LoginComponent
+  },
+
+  {
+    path: CreateAccountPath.createAccount,
+    pathMatch: 'full',
+    component: CreateAccount
+  },
+
+  {
     path: 'pending-approval',
     pathMatch: 'full',
     component: PendingApprovalsComponent
-   },
+  },
 
-  { path: 'resources',
-   pathMatch: 'full',
-   component: ResourcesComponent
+  {
+    path: 'resources',
+    pathMatch: 'full',
+    component: ResourcesComponent
+  },
+
+  {
+    path: 'analytics',            // ⭐ NEW ROUTE
+    pathMatch: 'full',
+    component: AnalyticsComponent
   }
+  
 
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {})],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
